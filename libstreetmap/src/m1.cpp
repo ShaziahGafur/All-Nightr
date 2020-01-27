a7e0278ca6ba2a2306a6bfee1e0748751b68585b
Saskia <saskia.george@mail.utoronto.ca>
2020-01-27 17:02:02 -0500

    Added stub implementations for all functions in m1.cpp

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -42,44 +42,77 @@ void close_map() {
     
 }
 
-double find_distance_between_two_points(std::pair<LatLon, LatLon> points);
+double find_distance_between_two_points(std::pair<LatLon, LatLon> points){
+    double distanceBetweenTwoPoints;
+    return distanceBetweenTwoPoints;
+}
 
 //Returns the length of the given street segment in meters
-double find_street_segment_length(int street_segment_id);
+double find_street_segment_length(int street_segment_id){
+    double streetSegmentLength;
+    return streetSegmentLength;
+}
 
 //Returns the travel time to drive a street segment in seconds 
 //(time = distance/speed_limit)
-double find_street_segment_travel_time(int street_segment_id);
+double find_street_segment_travel_time(int street_segment_id){
+    double streetSegmentTravelTime;
+    return streetSegmentTravelTime;
+}
 
 //Returns the nearest intersection to the given position
-int find_closest_intersection(LatLon my_position);
+int find_closest_intersection(LatLon my_position){
+    int closestIntersection;
+    return closestIntersection;
+}
 
 //Returns the street segments for the given intersection 
-std::vector<int> find_street_segments_of_intersection(int intersection_id);
+std::vector<int> find_street_segments_of_intersection(int intersection_id){
+    std::vector<int> ssOfIntersection;
+    return ssOfIntersection;
+}
 
 //Returns the street names at the given intersection (includes duplicate street 
 //names in returned vector)
-std::vector<std::string> find_street_names_of_intersection(int intersection_id);
+std::vector<std::string> find_street_names_of_intersection(int intersection_id){
+    std::vector<std::string> streetNamesOfIntersection;
+    return streetNamesOfIntersection;
+}
 
 //Returns true if you can get from intersection_ids.first to intersection_ids.second using a single 
 //street segment (hint: check for 1-way streets too)
 //corner case: an intersection is considered to be connected to itself
-bool are_directly_connected(std::pair<int, int> intersection_ids);
+bool are_directly_connected(std::pair<int, int> intersection_ids){
+    bool directlyConnected = true;
+    return directlyConnected;
+}
 
 //Returns all intersections reachable by traveling down one street segment 
 //from given intersection (hint: you can't travel the wrong way on a 1-way street)
 //the returned vector should NOT contain duplicate intersections
-std::vector<int> find_adjacent_intersections(int intersection_id);
+std::vector<int> find_adjacent_intersections(int intersection_id){
+    std::vector<int> adjacentIntersections;
+    return adjacentIntersections;
+}
 
 //Returns all street segments for the given street
-std::vector<int> find_street_segments_of_street(int street_id);
+std::vector<int> find_street_segments_of_street(int street_id){
+    std::vector<int> streetSegmentsOfStreet;
+    return streetSegmentsOfStreet;
+}
 
 //Returns all intersections along the a given street
-std::vector<int> find_intersections_of_street(int street_id);
+std::vector<int> find_intersections_of_street(int street_id){
+    std::vector<int> intersectionsOfStreet;
+    return intersectionsOfStreet;
+}
 
 //Return all intersection ids for two intersecting streets
 //This function will typically return one intersection id.
-std::vector<int> find_intersections_of_two_streets(std::pair<int, int> street_ids);
+std::vector<int> find_intersections_of_two_streets(std::pair<int, int> street_ids){
+    std::vector<int> intersectionsOfTwoStreets;
+    return intersectionsOfTwoStreets;
+}
 
 //Returns all street ids corresponding to street names that start with the given prefix
 //The function should be case-insensitive to the street prefix. You should ignore spaces.
@@ -88,14 +121,23 @@ std::vector<int> find_intersections_of_two_streets(std::pair<int, int> street_id
 //vector.
 //You can choose what to return if the street prefix passed in is an empty (length 0) 
 //string, but your program must not crash if street_prefix is a length 0 string.
-std::vector<int> find_street_ids_from_partial_street_name(std::string street_prefix);
+std::vector<int> find_street_ids_from_partial_street_name(std::string street_prefix){
+    std::vector<int> streetIdsFromPartialStreetName;
+    return streetIdsFromPartialStreetName;
+}
 
 //Returns the area of the given closed feature in square meters
 //Assume a non self-intersecting polygon (i.e. no holes)
 //Return 0 if this feature is not a closed polygon.
-double find_feature_area(int feature_id);
+double find_feature_area(int feature_id){
+    double featureArea;
+    return featureArea;
+}
 
 //Returns the length of the OSMWay that has the given OSMID, in meters.
 //To implement this function you will have to  access the OSMDatabaseAPI.h 
 //functions.
-double find_way_length(OSMID way_id);
+double find_way_length(OSMID way_id){
+    double wayLength;
+    return wayLength;
+}
