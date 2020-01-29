000b859be192c112262b86bfd6e9ee43aa7a65fe
Saskia <saskia.george@mail.utoronto.ca>
2020-01-29 11:18:55 -0500

    Re-pasted definition for load_map with more comments from Instructors.

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -23,15 +23,29 @@
 
 #include <cmath>
 
-bool load_map(std::string /*map_path*/) {
+// load_map will be called with the name of the file that stores the "layer-2"
+// map data (the street and intersection data that is higher-level than the
+// raw OSM data). This file name will always end in ".streets.bin" and you 
+// can call loadStreetsDatabaseBIN with this filename to initialize the
+// layer 2 API.
+// If you need data from the lower level, layer 1, API that provides raw OSM
+// data (nodes, ways, etc.) you will also need to initialize the layer 1 
+// OSMDatabaseAPI by calling loadOSMDatabaseBIN. That function needs the 
+// name of the ".osm.bin" file that matches your map -- just change 
+// ".streets" to ".osm" in the map_streets_database_filename to get the proper
+// name.
+bool load_map(std::string map_streets_database_filename) {
     bool load_successful = false; //Indicates whether the map has loaded 
                                   //successfully
+
+    std::cout << "load_map: " << map_streets_database_filename << std::endl;
 
     //
     //Load your map related data structures here
     //
 
     
+
     load_successful = true; //Make sure this is updated to reflect whether
                             //loading the map succeeded or failed
 
