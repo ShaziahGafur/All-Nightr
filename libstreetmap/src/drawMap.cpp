8d37f453220b8defe763c31b0c3ec9151d7ba0e7
Saskia <saskia.george@mail.utoronto.ca>
2020-02-22 17:19:55 -0500

    Implemented stubs draw_map function and draw_main_canvas functions

--- libstreetmap/src/drawMap.cpp
+++ libstreetmap/src/drawMap.cpp
@@ -2,6 +2,9 @@
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
 
+
+void draw_main_canvas (ezgl::renderer *g);
+
 void draw_map (){       
 ezgl::application::settings settings;
 settings.main_ui_resource="libstreetmap/resources/main.ui";
@@ -10,8 +13,17 @@ settings.canvas_identifier="MainCanvas";
 
 ezgl::application application(settings);
 
-
+ezgl:: rectangle initial_world({0,0},{1000,1000});
+application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
 
 
 application.run(NULL,NULL,NULL,NULL);            
+}
+
+void draw_main_canvas (ezgl::renderer *g){
+    g->draw_rectangle({0, 0}, {1000, 1000});
+    g->set_color (ezgl::BLACK);
+    g->set_color (0, 0, 0, 255);  // 8-bit r, g, b, alpha. alpha of 255 is opaque, 0 is transparent
+    g->set_line_width (3);   // 3 pixels wide
+    g->set_line_dash (ezgl::line_dash::asymmetric_5_3);
 }
