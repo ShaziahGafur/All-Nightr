093622caf32833d96135cf651e4a57626e8209fa
Shaziah <shaziah.gafur@mail.utoronto.ca>
2020-02-25 16:41:06 -0500

    Code cleaned, merged cartesian conversion

--- libstreetmap/src/drawMap.cpp
+++ libstreetmap/src/drawMap.cpp
@@ -17,14 +17,14 @@ float latAvg; //average latitude of map, value set in draw_map_blank_canvas
 void draw_map_blank_canvas ();
 void draw_main_canvas (ezgl::renderer *g);
 
-void draw_intersections();
+void draw_intersections();  
 
-void find_map_bounds(double& max_lat, double& min_lat, double& max_lon, double& min_lon);
+//void find_map_bounds(double& max_lat, double& min_lat, double& max_lon, double& min_lon);
 
-float x_from_lon(float lon);
-float y_from_lat(float lat);
-float lon_from_x(float x);
-float lat_from_y(float y);
+std::pair < double, double > latLonToCartesian (double lat, double lon);
+
+float y_from_lat (float lat);
+float x_from_lon (float lon);
 
 /************************************************/
 
@@ -49,6 +49,7 @@ void draw_map_blank_canvas (){
     double min_lon = max_lon;
     intersections.resize(numIntersections);
     
+    double sumLat = 0;
     for(int i = 0; i < numIntersections; i++){
         
         //get position from streetsdatabaseAPI function
@@ -61,11 +62,22 @@ void draw_map_blank_canvas (){
         min_lat = std::min(min_lat, intersections[i].position.lat());
         max_lon = std::max(max_lon, intersections[i].position.lon());
         min_lon = std::min(min_lon, intersections[i].position.lon());
+        
+        sumLat = sumLat + intersections[i].position.lat();
     }
     
-    latAvg = (max_lat + min_lat ) * 0.5; //sets value of global variable
+    //update global variable
+    latAvg = sumLat /** DEGREE_TO_RADIAN*// (numIntersections/*-1*/); //lat avg calculation fixed. Remove comments after looking it over
+            
+      //convert latlon points to cartesian points
+      //declare pair of min and Max cartesian coordinates and assign them to return pair of latlon to cartesian helper function
     
-    ezgl::rectangle initial_world({min_lon, min_lat},{max_lon, max_lat});
+//    std::pair < double, double > minCartesian = latLonToCartesian (min_lat, min_lon);
+//    std::pair < double, double > maxCartesian = latLonToCartesian (max_lat, max_lon);
+//    ezgl::rectangle initial_world({minCartesian.first, minCartesian.second},{maxCartesian.first, maxCartesian.second});
+    
+    ezgl::rectangle initial_world({min_lon, min_lat},{max_lon, max_lat}); //keep this initial_world version (refer to tutorial slides)
+    
     application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
     application.run(NULL,NULL,NULL,NULL);
 }
@@ -81,9 +93,11 @@ void draw_main_canvas (ezgl::renderer *g){
 
       float x = intersections[i].position.lon();
       float y = intersections[i].position.lat();
+
+      //must convert lat lon values to cartesian (refer to tutorial slides)
 //      x = x_from_lon(x);
 //      y = y_from_lat(y);
-            
+     
       float width = 0.001;
       float height = width;
 
@@ -92,72 +106,21 @@ void draw_main_canvas (ezgl::renderer *g){
     }
 }
 
-/**
- * Finds the maximum and minimum lat and lons of the map based on intersections
- */
-void find_map_bounds(double& max_lat, double& min_lat, double& max_lon, double& min_lon){
-    //Initializes maximum and minimum values to the values of the first intersection
-    max_lat = getIntersectionPosition(0).lat(); 
-    min_lat = max_lat;
-    max_lon = getIntersectionPosition(0).lon();
-    min_lon = max_lon;
-        
-//    
-//    //loops through all intersections, searches for the max & min lat & lons in the entire map and assigning them to the double pointers
-    for (int intersectionId = 0; intersectionId < getNumIntersections(); intersectionId++){
-        
-        max_lat = std::max(max_lat, intersections[intersectionId].position.lat());
-        min_lat = std::min(min_lat, intersections[intersectionId].position.lat());
-        max_lon = std::max(max_lon, intersections[intersectionId].position.lon());
-        min_lon = std::min(min_lon, intersections[intersectionId].position.lon());
-    }
+std::pair < double, double > latLonToCartesian (double lat, double lon){
+    //convert LatLon points into x y coordinates
+    double y = lat*DEGREE_TO_RADIAN *EARTH_RADIUS_METERS;
+    double x = lon*DEGREE_TO_RADIAN *EARTH_RADIUS_METERS*cos(latAvg);
+    std::pair < double, double> cartesian (x, y);
+    return cartesian;
 }
 
-float x_from_lon(float lon){
-    return lon*DEGREE_TO_RADIAN *cos(latAvg); 
+float x_from_lon (float lon){
+    //convert LatLon points into x y coordinates
+//    double y = lat*DEGREE_TO_RADIAN *EARTH_RADIUS_METERS;
+    return lon*DEGREE_TO_RADIAN *EARTH_RADIUS_METERS*cos(latAvg);
 }
 
-float y_from_lat(float lat){
-    return lat*DEGREE_TO_RADIAN;
+float y_from_lat (float lat){
+    //convert LatLon points into x y coordinates
+    return lat*DEGREE_TO_RADIAN *EARTH_RADIUS_METERS;
 }
-
-float lon_from_x(float x){
-    return 0;
-}
-
-float lat_from_y(float y){
-    return 0;
-}
-
-
-//void populateIntersections(){
-//    
-//    int numIntersections = getNumIntersections();
-//        
-//    intersections.resize(numIntersections);
-//    
-//    for(int i = 0; i < numIntersections; i++){
-//        
-//        //get position from streetsdatabaseAPI function
-//        intersections[i].position = getIntersectionPosition(i);
-//        
-//        //get name from streetsdatabaseAPI function
-//        intersections[i].name = getIntersectionName(i);
-//        std::cout<<"\nIntersection # "<<i<<intersections[i].position.lat();
-//    }
-//}
-
-/*void draw_intersections(ezgl::renderer *g){
-    for(size_t i = 0; i < intersections.size(); ++i){
-        
-        float x = intersections[i].position.lon();
-        float y = intersections[i].position.lat();
-        
-        float width = 1;
-        
-        float height = width;
-        
-        g.fill_rectangle({x,y}, {x + width, y + height});
-    
-    }
-}*/
