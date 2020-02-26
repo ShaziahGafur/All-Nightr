295d081a8f1613943468441c9e4316c6718d8474
Maggie <maggie.ibarradiaz@mail.utoronto.ca>
2020-02-26 01:03:42 -0500

    Edited drawMap.cpp by adding the Feature drawing functionality!

--- libstreetmap/src/drawMap.cpp
+++ libstreetmap/src/drawMap.cpp
@@ -5,6 +5,7 @@
 #include "m2.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
+#include "ezgl/point.hpp"
 #include "intersection_data.h"
 #include <vector>
 #include <string>
@@ -86,16 +87,16 @@ void draw_map_blank_canvas (){
     
     //update the global variable with calculated lat average
     latAvg = sumLat/(numIntersections);
-    std::cout << latAvg;
             
     //convert latlon points to cartesian points
     //declare pair of min and Max cartesian coordinates and assign them to return pair of latlon to cartesian helper function
     
-    std::pair < double, double > minCartesian = latLonToCartesian (min_lat, min_lon);
-    std::pair < double, double > maxCartesian = latLonToCartesian (max_lat, max_lon);
+//    std::pair < double, double > minCartesian = latLonToCartesian (min_lat, min_lon);
+//    std::pair < double, double > maxCartesian = latLonToCartesian (max_lat, max_lon);
     //ezgl::rectangle initial_world({minCartesian.first, minCartesian.second},{maxCartesian.first, maxCartesian.second});
     
     ezgl::rectangle initial_world({min_lon, min_lat},{max_lon, max_lat}); //keep this initial_world version (refer to tutorial slides)
+    //  ezgl::rectangle initial_world({min_lon, min_lat},{max_lon, max_lat});
     
     application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
     application.run(NULL,NULL,NULL,NULL);
@@ -109,48 +110,129 @@ void draw_main_canvas (ezgl::renderer *g){
     g->set_line_width (3);   // 3 pixels wide
     g->set_line_dash (ezgl::line_dash::asymmetric_5_3);
     
-    //Drawing Intersections
-    //***********************************************************************************
+//    //Drawing Intersections
+//    //***********************************************************************************
+//    
+//    for(size_t i = 0; i < intersections.size(); ++i){
+//
+//      float x = intersections[i].position.lon();
+//      float y = intersections[i].position.lat();
+//
+//      //must convert lat lon values to cartesian (refer to tutorial slides)
+////      x = x_from_lon(x);
+////      y = y_from_lat(y);
+//     
+//      float width = 0.0005;
+//      float height = width;
+//
+//      g->fill_rectangle({x,y}, {x + width, y + height});
+//    
+//    }
+//    
+////Drawing Streets
+////***********************************************************************************
+//    
+//    g->set_color (255, 255, 0, 255);
+//    
+//    for (int streetIdx = 0; streetIdx < StreetVector.size(); streetIdx++ ){ //for each street
+//        std::vector<int> segments = StreetVector[streetIdx].streetSegments;
+//        for (int segmentId = 0; segmentId < segments.size(); segmentId++ ){
+//            struct InfoStreetSegment segmentInfo = getInfoStreetSegment(segments[segmentId]);
+//            if (segmentInfo.curvePointCount==0){
+//                int fromIntersection = segmentInfo.from; 
+//                int toIntersection = segmentInfo.to; 
+//                
+//                float xF = intersections[fromIntersection].position.lon();
+//                float yF = intersections[fromIntersection].position.lat();
+//                
+//                float xT = intersections[toIntersection].position.lon();
+//                float yT = intersections[toIntersection].position.lat();
+//                g->draw_line({xF, yF}, {xT, yT});
+//            }
+//        }
+//    }
+//    
+//Drawing Features
+//***********************************************************************************
+    g->set_line_dash(ezgl::line_dash::none);
     
-    for(size_t i = 0; i < intersections.size(); ++i){
+    //variables needed for drawing features
+    double previousPoint_x, previousPoint_y, point_x, point_y;
+    
+    for(size_t featureId = 0; featureId < getNumFeatures(); ++featureId){
+        //get feature type to determine colour
+        FeatureType feature_type = getFeatureType(featureId);
+        switch(feature_type)
+        {
+            case     Unknown   : g->set_color (224,224,224, 255);
+                                 break;
+            case     Park      : g->set_color (204,255,204, 255);
+                                 break;
+            case     Beach     : g->set_color (245,230,194, 255);
+                                 break;       
+            case     Lake      : g->set_color (204,255,255, 255);
+                                 break;
+            case     River     : g->set_color (177,229,229, 255);
+                                 break;
+            case     Island    : g->set_color (128,243,151, 255);
+                                 break;
+            case     Building  : g->set_color (220,220,220, 255);
+                                 break;
+            case     Greenspace: g->set_color (128,243,151, 255);
+                                 break;
+            case     Golfcourse: g->set_color (0,255,128, 255);
+                                 break;
+            case     Stream    : g->set_color (177,229,229, 255);
+                                 break;
+            
+            default: g->set_color (224,224,224, 255);
+        }
+        
+        //get all of the points for that particular feature
+        int numOfFeaturePoints = getFeaturePointCount(featureId);
+        
+        //find first and last points and check if features is polygon or polyline
+        LatLon firstPoint = getFeaturePoint(0, featureId);  
+        LatLon lastPoint = getFeaturePoint(numOfFeaturePoints-1, featureId); 
 
-      float x = intersections[i].position.lon();
-      float y = intersections[i].position.lat();
+        // If the first point and the last point (getFeaturePointCount-1) are NOT the same location, the feature is a polyline
+        if (FeatureAreaVector[featureId] == 0) {
+            
+            //iterate through feature points to get (x,y) coordinates  in order to draw the polyline)
+            for (unsigned featurePointId = 1; featurePointId < numOfFeaturePoints; featurePointId++){
+                //Declare and initialize adjacent LatLon points
+                LatLon previousPoint = getFeaturePoint(featurePointId-1, featureId);
+                LatLon point = getFeaturePoint(featurePointId, featureId);
+            
+//                //convert LatLon points into x y coordinates
+//                previousPoint_x = x_from_lon (previousPoint.lon());
+//                previousPoint_y = y_from_lat (previousPoint.lat());
+//
+//                point_x = x_from_lon (point.lon());
+//                point_y = y_from_lat (point.lat());
 
-      //must convert lat lon values to cartesian (refer to tutorial slides)
-//      x = x_from_lon(x);
-//      y = y_from_lat(y);
-     
-      float width = 0.0001;
-      float height = width;
-
-      g->fill_rectangle({x,y}, {x + width, y + height});
-    
-    }
-    
-    //Drawing Streets
-     //***********************************************************************************
-    
-    g->set_color (255, 255, 0, 255);
-    
-    for (int streetIdx = 0; streetIdx < StreetVector.size(); streetIdx++ ){ //for each street
-        std::vector<int> segments = StreetVector[streetIdx].streetSegments;
-        for (int segmentId = 0; segmentId < segments.size(); segmentId++ ){
-            struct InfoStreetSegment segmentInfo = getInfoStreetSegment(segments[segmentId]);
-            if (segmentInfo.curvePointCount==0){
-                int fromIntersection = segmentInfo.from; 
-                int toIntersection = segmentInfo.to; 
-                
-                float xF = intersections[fromIntersection].position.lon();
-                float yF = intersections[fromIntersection].position.lat();
-                
-                float xT = intersections[toIntersection].position.lon();
-                float yT = intersections[toIntersection].position.lat();
-                g->draw_line({xF, yF}, {xT, yT});
+                //draw line between feature points
+                g->set_line_width(3);
+                g->draw_line({previousPoint.lon(), previousPoint.lat()}, {point.lon(), point.lat()});
             }
         }
-    }  
-    
+        else{
+            std::vector<ezgl::point2d> points_xy_coordinates;
+            
+            //iterate through feature points to get (x,y) coordinates  in order to draw the polygon
+            for (unsigned featurePointId = 0; featurePointId < numOfFeaturePoints; featurePointId++){
+
+//                //convert LatLon points into (x, y) coordinate pairs
+//                point_x = x_from_lon (getFeaturePoint(featurePointId, featureId).lon());
+//                point_y = y_from_lat (getFeaturePoint(featurePointId, featureId).lat());
+
+                points_xy_coordinates.push_back(ezgl::point2d(getFeaturePoint(featurePointId, featureId).lon(), getFeaturePoint(featurePointId, featureId).lat()));
+            }
+            
+       //     ezgl function which takes a vector<point2d> (i.e (x,y) coordinates for each point that defines the feature outline)
+            g->fill_poly(points_xy_coordinates);
+        }
+    }
 }
 
 std::pair < double, double > latLonToCartesian (double lat, double lon){
