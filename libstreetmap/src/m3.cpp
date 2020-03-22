2f35d1b5619a8974608b3a97aa9bf60af09d6e0c
Saskia <saskia.george@mail.utoronto.ca>
2020-03-22 14:42:25 -0400

    Implemented compute_path_travel_time function

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -4,11 +4,57 @@
  * and open the template in the editor.
  */
 #include "m3.h"
+#include "StreetsDatabaseAPI.h"
+#include "m1.h"
+#include "m1.cpp"
 
-double compute_path_travel_time(const std::vector<StreetSegmentIndex>& path, 
-                                const double turn_penalty){
-    double zero = 1;
-    return zero;
+// Returns the time required to travel along the path specified, in seconds.
+// The path is given as a vector of street segment ids, and this function can
+// assume the vector either forms a legal path or has size == 0.  The travel
+// time is the sum of the length/speed-limit of each street segment, plus the
+// given turn_penalty (in seconds) per turn implied by the path.  If there is
+// no turn, then there is no penalty. Note that whenever the street id changes
+// (e.g. going from Bloor Street West to Bloor Street East) we have a turn.
+double compute_path_travel_time(const std::vector<StreetSegmentIndex>& path, const double turn_penalty){
+    
+    double travelTime = 0;
+    //Error check if vector size is zero
+    if (path.size() == 0){
+        return travelTime;
+    }
+    
+    //make streetinfo struct and streetID variable to retrieve streetID from street segment index
+    InfoStreetSegment segStruct;
+    int previousStreetID, nextStreetID;
+    
+    //get first streetID
+    segStruct = getInfoStreetSegment(path.front());
+    previousStreetID = segStruct.streetID;
+    
+    //Find number of turn penalties by finding # of turns -> Need to find street id from street segment to get # of turns
+    //Use global structure?
+    //Also get travel time of each segment and store in travelTime
+    std::vector<StreetSegmentIndex>::const_iterator it = path.begin();
+    
+    do{
+        // move to next segment
+        it++; 
+        
+        //add travel time of this segment        
+        travelTime = travelTime + find_street_segment_travel_time(*it);
+        
+        //get street id
+        segStruct = getInfoStreetSegment(*it);
+        nextStreetID = segStruct.streetID;
+        
+        //check if streetID has changed, if yes -> add turn penalty and increment previous streetID
+        if (previousStreetID != nextStreetID){
+            travelTime = travelTime + turn_penalty;
+            previousStreetID = nextStreetID;
+        } 
+    } while (it != path.end());
+ 
+    return travelTime;
 }
         
  std::vector<StreetSegmentIndex> find_path_between_intersections(
