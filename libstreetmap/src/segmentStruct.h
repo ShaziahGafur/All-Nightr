765bab889f0f164ee385c81b3085f72d022e06ea
Saskia <saskia.george@mail.utoronto.ca>
2020-03-25 15:45:16 -0400

    Made new class to hold indo about which segments to highlight

---
+++ libstreetmap/src/segmentStruct.h
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   segmentStruct.h
+ * Author: georg157
+ *
+ * Created on March 25, 2020, 2:57 PM
+ */
+
+#ifndef SEGMENTSTRUCT_H
+#define SEGMENTSTRUCT_H
+
+class segmentStruct{
+    public:
+        segmentStruct();
+        
+        ~segmentStruct();
+        
+        bool walking;
+        
+        bool driving;
+    
+};
+
+#endif /* SEGMENTSTRUCT_H */
+
